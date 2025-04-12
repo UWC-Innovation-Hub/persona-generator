7eae8698a979593d004c12f2f7ee1ba4b66dacb7
UWC Innovation Hub Immersive Zone <134297825+UWC-UIH-Dev@users.noreply.github.com>
2025-04-12 11:54:37 +0200

    Persona Generator

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PersonaGenerator from './components/PersonaGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonaGenerator />
+    </div>
+  );
+}
+
+export default App;
